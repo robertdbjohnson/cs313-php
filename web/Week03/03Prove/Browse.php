fb334e760b1847b97e15e13db7dfd5f4c0e8ff3d
robertdbjohnson <joh16151@byui.edu>
2020-01-25 21:09:53 -0700

    Added some HTML and CSS stuff

--- web/Week03/03Prove/Browse.php
+++ web/Week03/03Prove/Browse.php
@@ -19,11 +19,11 @@
         <div class="content">
             <h2>So cheap, you're basically robbing us</h2>
             <p>Items:</p>
-            <button class="button button2">Green</button>DSLR Camera $100.00<br>
-            <button class="button button2">Blue</button>Jacket $50.00<br>
-            <button class="button button2">Red</button>Water Bottle $10.00<br>
-            <button class="button button2">Gray</button>Sports Watch $15.00<br>
-            <button class="button button2">Black</button>Laptop $250.00<br>
+            <button class="button button2">Add to cart</button>DSLR Camera $100.00
+            <button class="button button2">Add to cart</button>Jacket $50.00
+            <button class="button button2">Add to cart</button>Water Bottle $10.00
+            <button class="button button2">Add to cart</button>Sports Watch $15.00
+            <button class="button button2">Add to cart</button>Laptop $250.00
         </div>
 
         <div class="footer">
