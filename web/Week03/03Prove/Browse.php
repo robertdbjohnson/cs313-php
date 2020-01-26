f5a58ab246f87218bf8afaec716ce41345861589
robertdbjohnson <joh16151@byui.edu>
2020-01-25 20:35:48 -0700

    Starting on 03Prove

---
+++ web/Week03/03Prove/Browse.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Rober Co.</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="03Prove.css">
+    </head>
+    <body>
+        <h1>RoberCo</h1>
+        <div class="topnav">
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+        </div>
+
+        <div class="content">
+            <h2>CSS Template</h2>
+            <p>A topnav, content and a footer.</p>
+        </div>
+
+        <div class="footer">
+            <p>Footer</p>
+        </div>
+        <script src="" async defer></script>
+    </body>
+</html>
