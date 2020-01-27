9bbff7607974c4c4aa7dcbc9935f9e6f6f68f645
robertdbjohnson <joh16151@byui.edu>
2020-01-26 21:23:12 -0700

    Adding some PHP to 03Prove

--- web/Week03/03Prove/ViewCart.php
+++ web/Week03/03Prove/ViewCart.php
@@ -25,14 +25,14 @@ session_start();
             <button class="button button3">Remove from cart</button>DSLR Camera.....$100.00
             <p>Quantity: <?php echo $_SESSION["camera"]; ?></p><br><hr>
             <button class="button button3">Remove from cart</button>Jacket.....$50.00<br>
-            <p style="text-align:right;">Quantity: <?php echo $_SESSION["jacket"]; ?></p><br><hr>
+            <p>Quantity: <?php echo $_SESSION["jacket"]; ?></p><br><hr>
             <button class="button button3">Remove from cart</button>Water Bottle.....$10.00<br>
-            <p style="text-align:right;">Quantity: <?php echo $_SESSION["bottle"]; ?></p><br><hr>
+            <p>Quantity: <?php echo $_SESSION["bottle"]; ?></p><br><hr>
             <button class="button button3">Remove from cart</button>Sports Watch.....$15.00<br>
-            <p style="text-align:right;">Quantity: <?php echo $_SESSION["watch"]; ?></p><br><hr>
+            <p>Quantity: <?php echo $_SESSION["watch"]; ?></p><br><hr>
             <button class="button button3">Remove from cart</button>Laptop.....$250.00<br><br>
-            <p style="text-align:right;">Quantity: <?php echo $_SESSION["laptop"]; ?></p><br><hr>
-            <button style="text-align:center;" class="button button1">Checkout</button><br>
+            <p>Quantity: <?php echo $_SESSION["laptop"]; ?></p><br><hr>
+            <p style="text-align:center;"><button class="button button1">Checkout</button></p><br>
         </div>
 
         <div class="footer">
