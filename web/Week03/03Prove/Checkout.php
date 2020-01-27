937c566747e46d63f971f67e55d4ae7a0c589ff8
robertdbjohnson <joh16151@byui.edu>
2020-01-26 23:26:53 -0700

    Starting work on Checkout.php

--- web/Week03/03Prove/Checkout.php
+++ web/Week03/03Prove/Checkout.php
@@ -19,11 +19,10 @@ session_start();
         $street = $city = $state = $zip = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = test_input($_POST["name"]);
+        $street = test_input($_POST["street"]);
         $city = test_input($_POST["city"]);
-        $website = test_input($_POST["website"]);
-        $zip = test_input($_POST["zip"]);
         $state = test_input($_POST["state"]);
+        $zip = test_input($_POST["zip"]);
         }
 
         function test_input($data) {
