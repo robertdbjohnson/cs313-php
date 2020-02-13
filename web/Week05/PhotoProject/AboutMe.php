63f6e180f2aa5f32171a0adef2399d70737ced60
robertdbjohnson <joh16151@byui.edu>
2020-02-12 23:50:22 -0700

    Adding some pictures to project

--- web/Week05/PhotoProject/AboutMe.php
+++ web/Week05/PhotoProject/AboutMe.php
@@ -10,7 +10,7 @@ session_start();
         <title>Robert Co.</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="PhotoProject.css">
+        <link rel="stylesheet" href="PhotoPoject.css">
     </head>
     <body>
         <h1>RobertCo</h1>
