47e0e65942bdbab7a5b891ae2d99e441eb4a388e
robertdbjohnson <joh16151@byui.edu>
2020-02-13 20:27:03 -0700

    Adding some pictures to project

--- web/Week05/PhotoProject/AboutMe.php
+++ web/Week05/PhotoProject/AboutMe.php
@@ -13,7 +13,7 @@ session_start();
         <link rel="stylesheet" href="PhotoProject.css">
     </head>
     <body>
-        <h1>RobertCo</h1>
+        <h1>PhotoCo</h1>
         <div class="topnav">
             <a href="https://desolate-fjord-07032.herokuapp.com/Week05/PhotoProject/Homepage.php">Homepage</a>
             <a href="https://desolate-fjord-07032.herokuapp.com/Week05/PhotoProject/Pictures.php">Pictures</a>
@@ -33,6 +33,13 @@ session_start();
                 <div class="img-container">
                     <img src="Filippino Flag (2).jpg" alt="Filippino Flag">
                 </div>
+            </div>
+            <div>
+                <p>I am currently a college student studing software engineering at Brigham Young University. Before studying in Idaho, I was born in the Philippines, but grow up in the Dallas, Texas area. 
+                Growing up my famlily has always been important to me and always will be. They have always been there for me and I hope I can always be there for them. I am a fan of 
+                video games, stock trading, staying active, photography, and so much more. I have only recently started getting into photography this year and I don't claim to be the best
+                photographer in the world and am in fact far from it. I love taking my camera out (her name is Sally) and taking pictures of fun things like landscapes, architecture , but especially people.
+                I think being able to capture a special moment in a great picture is priceless.</p>
             </div>
         </div>
 
