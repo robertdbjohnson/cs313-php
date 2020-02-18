6d59045199f02903ecb6f1820d3c1c7b5dbb7efa
robertdbjohnson <joh16151@byui.edu>
2020-02-18 07:44:50 -0700

    Working Pictures Page

--- web/Week05/PhotoProject/Pictures.php
+++ web/Week05/PhotoProject/Pictures.php
@@ -53,7 +53,7 @@ session_start();
                     <img src="SnowAtPorter.JPG" alt="Filippino Flag">
                 </div><br>
 
-                <img src="NorthernAZBorderThin.JPG" alt="Filippino Flag">
+                <img src="NorthernAZBorderThin.jpg" alt="Filippino Flag">
 
                 <div class="img-container">
                     <img src="PinkFlower.JPG" alt="Jumping Picture after skydiving"> 
